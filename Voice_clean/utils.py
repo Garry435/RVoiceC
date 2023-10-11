c51ecc676567468243ff5a2e83450593a0a81a22
Garry435 <143331750+Garry435@users.noreply.github.com>
2023-10-11 11:42:22 +0530

    Update utils.py

--- Voice_clean/utils.py
+++ Voice_clean/utils.py
@@ -1,7 +1,7 @@
 from Voice_clean.mdx import run_mdx
 import json
 def preprocess_audio(orig_audio_path,mdxnet_models_dir='Voice_clean/mdxnet_models',audio_output_dir='Voice_clean/audio_outputs'):
-    with open('mdxnet_models/model_data.json') as infile:
+    with open('Voice_clean/mdxnet_models/model_data.json') as infile:
         mdx_model_params = json.load(infile)
     print('[~] Cleaning Vocals')
     vocals_path = run_mdx(mdx_model_params, audio_output_dir, os.path.join(mdxnet_models_dir, 'UVR-MDX-NET-Voc_FT.onnx'), orig_audio_path, denoise=True,m_threads=6)
